f0c6c4a4f6c26d769c0b1e6b5093b77bb0957683
NikNik <nickkru@gmail.com>
2022-11-21 20:05:55 +0300

    fix log.csv

--- logger.py
+++ logger.py
@@ -3,11 +3,11 @@
 """
 
 from _datetime import datetime as dt
-from time import time
+# from time import time
 
 
 def log_operation(data):
     time = dt.now().strftime('%H:%M:%S')
-    with open('log\log.csv', 'a') as file:
+    with open('log.csv', 'a') as file:
         file.write('[{}]: {}\n'
                    .format(time, data))
