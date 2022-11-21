d64355c20d5ba961fc39b831ff5811bc358136b2
NikNik <nickkru@gmail.com>
2022-11-21 18:59:55 +0300

    добавлен Модуль логирования и файла лог

--- logger.py
+++ logger.py
@@ -3,11 +3,11 @@
 """
 
 from _datetime import datetime as dt
-import time
+from time import time
 
 
 def log_operation(data):
-    time_ = dt.now().strftime('%H:%M:%S')
+    time = dt.now().strftime('%H:%M:%S')
     with open('log\log.csv', 'a') as file:
         file.write('[{}]: {}\n'
-                   .format(time_, data))
+                   .format(time, data))
